41a584b1e5f393e290951420b566e5c17ac9da81
ARukhadze <44447352+AlRukhadze@users.noreply.github.com>
2020-04-15 00:00:58 +0300

    Updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -19,17 +19,8 @@ def index():							# и соответствующий ему обработчи
 def wrong_add():
     return render_template('wrong_add.html')
 
-@app.route('/add_new', methods=['GET', 'POST'])
-# def add_new():
-#     # form = DeadlineForm()
-#     return render_template('add_form.html')
-
-# @app.route('open_cashier_menu', methods=['POST'])
-# def open_menu():
-#     global
-
-@app.route('/add_sale', methods=['GET', 'POST'])
-def add_sale():
+@app.route('/try_login', methods=['POST'])
+def try_login():
     global _login, _password
     _login = request.form['login']
     _password = request.form['password']
@@ -38,19 +29,26 @@ def add_sale():
     if _login == 'cashier_1' and _password == '1234':
         print("logged as a cashier")
         current_user = Login(_login, _password, 1)
-        return render_template('cashier_menu', cur_user=current_user)
+        return redirect('cashier_menu')
+        # return render_template('cashier_menu', cur_user=current_user)
     elif _login == 'administrator_1' and _password == '4321':
-        print("logged as a cashier")
+        print("logged as an administrator")
         current_user = Login(_login, _password, 0)
-        return render_template('administrator_menu', cur_user=current_user)
+        return redirect('administrator_menu')
+        # return render_template('administrator_menu', cur_user=current_user)
     else:
         return redirect('wrong_add')
         # переход на страницу с оповещением о некорректности ввода
 
+@app.route('/cashier_menu')
+def cashier_menu():
+    return render_template('cashier_menu.html')
 
-@app.route('/show_sales')
-def show_new():
+@app.route('/administrator_menu')
+def administrator_menu():
+    return render_template('administrator_menu.html')
+
+@app.route('/show_sales', methods=['POST'])
+def show_sales():
     list_of_sales = Sales.query.all()    # получаем лист наших дедлайнов (объектов типа Deadline)
-    # list_of_titles = [str(list_of_deadlines.d_title)]
-    # list_of_details = [str(list_of_deadlines.d_details)]
-    return render_template('show_deadlines.html', cur_list=list_of_deadlines)
+    return render_template('show_sales.html', cur_list=list_of_sales)
